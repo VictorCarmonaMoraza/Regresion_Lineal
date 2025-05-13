a0e97ef911f03c6ae78f76a107f03b8f86197766
Victor Carmona <victorcarmona9035.com>
2025-05-13 19:55:21 +0200

    Visualizacion del conjunto de datos

--- app.py
+++ app.py
@@ -16,4 +16,13 @@ print(f'la longitud del conjunto de datos costes {len(Y)}')
 Visualizacion del conjunto de datos
 necesario importacion de matplotlib.pyplot
 '''
+import matplotlib.pyplot as plt
+## %matplotlib inline
 
+plt.figure(figsize=(10, 8))  # Aumenta el ancho a 10 pulgadas y el alto a 8 pulgadas
+plt.plot(X, Y, "b.")
+plt.xlabel("Número de equipos afectados (u/1000)")
+plt.ylabel("Coste de los incidentes (u/10000)")
+plt.title("Relación entre equipos afectados y coste de incidentes")
+plt.grid(True)
+plt.show()
